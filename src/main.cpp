eda188e2e8545a69099f8ea6c78581b23b93e739
Kojo Bailey <kojomolojo@gmail.com>
2024-11-02 00:35:26 +0300

    Various changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <nucc/xfbin.hpp>
 #include <nucc/chunks/binary/asbr.hpp>
-#include <nucc/hash.hpp>
 
 template<typename... Args> void log(std::format_string<Args...> fmt, Args&&... args) {
     std::cout << "> " << std::format(fmt, std::forward<Args>(args)...) << std::endl;
@@ -8,6 +7,9 @@ template<typename... Args> void log(std::format_string<Args...> fmt, Args&&... a
 void log(std::string& str) {
     std::cout << "> " << str << std::endl;
 }
+
+// CONFIGS
+static int json_spacing = 2;
 
 int main(int argc, char* argv[]) {
     std::filesystem::path xfbin_path;
@@ -23,7 +25,91 @@ int main(int argc, char* argv[]) {
     }
 
     nucc::XFBIN xfbin{xfbin_path};
-    nucc::Binary buffer{xfbin.fetch<nucc::Binary>("messageInfo")};
-    nucc::ASBR::messageInfo message_info{buffer.data()};
-    log(message_info.entries[nucc::hash("5grn01_btlst_00_5bct01")].message);
+    std::filesystem::path main_directory = xfbin.name;
+    std::filesystem::create_directory(main_directory);
+    
+    nlohmann::ordered_json index_json;
+    index_json["External Name"] = xfbin.name;
+    index_json["Version"] = xfbin.version;
+    index_json["Game"] = xfbin.game_as_string();
+    for (auto& type : xfbin.index.types) {
+        index_json["Types"].push_back(type);
+    }
+    for (auto& path : xfbin.index.paths) {
+        index_json["Paths"].push_back(path);
+    }
+    for (auto& name : xfbin.index.names) {
+        index_json["Names"].push_back(name);
+    }
+
+    size_t page_count = 0;
+    for (auto& page : xfbin.pages) {
+        std::filesystem::path page_path;
+        std::filesystem::path page_directory;
+        nlohmann::ordered_json page_json;
+        page_json["Version"] = page.version;
+        page_json["Unknown"] = page.unk;
+        page_json["Chunk Map Offset"] = page.content.map_offset;
+        page_json["Extra Map Offset"] = page.content.extra_offset;
+
+        for (auto& chunk : page.chunks) {
+            if (chunk.type != nucc::Chunk_Type::Null) {
+                page_path = std::format("{:03} - {} ({})", page_count, chunk.name, chunk.type_as_string());
+                page_directory = main_directory / page_path;
+                std::filesystem::create_directory(page_directory);
+                break;
+            }
+        }
+
+        size_t chunk_index = 0;
+        for (auto& chunk : page.chunks) {
+            auto& page_json_chunk = page_json["Chunks"][chunk_index];
+            page_json_chunk["Type"] = chunk.type_as_string();
+            if (chunk.path != "")
+                page_json_chunk["Path"] = chunk.path;
+            if (chunk.name != "")
+                page_json_chunk["Name"] = chunk.name;
+            page_json_chunk["Version"] = chunk.version;
+            page_json_chunk["Unknown"] = chunk.unk;
+
+            index_json["Pages"][page_count][chunk_index] = page_json["Chunks"][chunk_index];
+
+            if (chunk.type == nucc::Chunk_Type::Null) {
+                std::ofstream output(page_directory / std::format("{:03} - null", chunk_index));
+                output.close();
+            } else if (chunk.type == nucc::Chunk_Type::Binary) {
+                nucc::Binary buffer{&chunk};
+                auto parse_binary = [&]<typename T>(T* t) {
+                    T binary_data{buffer.data()};
+                    std::ofstream output(page_directory / std::format("{:03} - {}.json", chunk_index, chunk.name));
+                    output << binary_data.write_to_json().dump(json_spacing);
+                    output.close();
+                };
+
+                if (chunk.name == "messageInfo") {
+                    parse_binary((nucc::ASBR::messageInfo*)nullptr);
+                } else if (chunk.name == "PlayerColorParam") {
+                    parse_binary((nucc::ASBR::PlayerColorParam*)nullptr);
+                } else if (chunk.name == "SpeakingLineParam") {
+                    parse_binary((nucc::ASBR::SpeakingLineParam*)nullptr);
+                } else if (chunk.name == "MainModeParam") {
+                    parse_binary((nucc::ASBR::MainModeParam*)nullptr);
+                } else {
+                    buffer.dump().dump_file((page_directory / std::format("{:03} - {}.binary", chunk_index, chunk.name)).string());
+                }
+            } else {
+                chunk.dump().dump_file((page_directory / std::format("{:03} - {}.binary", chunk_index, chunk.name)).string());
+            }
+            chunk_index++;
+        }
+        
+        std::ofstream page_file(page_directory / "_page.json");
+        page_file << page_json.dump(json_spacing);
+        page_file.close();
+        page_count++;
+    }
+
+    std::ofstream index_file(main_directory / "_index.json");
+    index_file << index_json.dump(json_spacing);
+    index_file.close();
 }
