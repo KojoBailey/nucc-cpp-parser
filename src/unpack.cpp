a18253fa4ff71f6ad0dcf295d70d2150340c9424
Kojo Bailey <kojomolojo@gmail.com>
2024-12-31 11:24:26 +0300

    Fix unpacking for messageInfo

--- src/unpack.cpp
+++ src/unpack.cpp
@@ -1,10 +1,14 @@
 #include "main.hpp"
 
 void Unpack_XFBIN(std::filesystem::path& xfbin_path) {
+    // Load XFBIN into class.
     nucc::XFBIN xfbin{xfbin_path};
+
+    // Create directory for XFBIN unpacked contents.
     std::filesystem::path main_directory = xfbin.name;
     std::filesystem::create_directory(main_directory);
     
+    // Write `_index.json` information.
     nlohmann::ordered_json index_json;
     index_json["External Name"] = xfbin.name;
     index_json["Version"] = xfbin.version;
@@ -19,16 +23,21 @@ void Unpack_XFBIN(std::filesystem::path& xfbin_path) {
         index_json["Names"].push_back(name);
     }
 
+    // For each XFBIN page, create a separate directory.
     size_t page_count = 0;
     for (auto& page : xfbin.pages) {
         std::filesystem::path page_path;
         std::filesystem::path page_directory;
+
+        // Write `_page.json` information.
         nlohmann::ordered_json page_json;
         page_json["Version"] = page.version;
         page_json["Unknown"] = page.unk;
         page_json["Chunk Map Offset"] = page.content.map_offset;
         page_json["Extra Map Offset"] = page.content.extra_offset;
 
+        // Create page directory, using first non-null chunk as title.
+        // (Will change later to prioritise `nuccChunkClump`.)
         for (auto& chunk : page.chunks) {
             if (chunk.type != nucc::Chunk_Type::Null) {
                 page_path = std::format("{:03} - {} ({})", page_count, chunk.name, chunk.type_as_string());
@@ -40,6 +49,7 @@ void Unpack_XFBIN(std::filesystem::path& xfbin_path) {
 
         size_t chunk_index = 0;
         for (auto& chunk : page.chunks) {
+            // Write each chunk's data to `_page.json`.
             auto& page_json_chunk = page_json["Chunks"][chunk_index];
             page_json_chunk["Type"] = chunk.type_as_string();
             if (chunk.path != "")
@@ -49,8 +59,10 @@ void Unpack_XFBIN(std::filesystem::path& xfbin_path) {
             page_json_chunk["Version"] = chunk.version;
             page_json_chunk["Unknown"] = chunk.unk;
 
+            // Update `_index.json` to have total page count.
             index_json["Pages"][page_count][chunk_index] = page_json["Chunks"][chunk_index];
 
+            // Create a file for each chunk.
             if (chunk.type == nucc::Chunk_Type::Null) {
                 std::ofstream output(page_directory / std::format("{:03} - null", chunk_index));
                 output.close();
@@ -59,12 +71,15 @@ void Unpack_XFBIN(std::filesystem::path& xfbin_path) {
                 auto Parse_Binary = [&]<typename T>(T* t) {
                     T binary_data{buffer.data()};
                     std::ofstream output(page_directory / std::format("{:03} - {}.json", chunk_index, chunk.name));
-                    output << binary_data.write_to_json("messageInfo_hashlist.json").dump(json_spacing);
+                    output << binary_data.write_to_json().dump(json_spacing);
                     output.close();
                 };
 
                 if (std::regex_match(chunk.path, std::regex(nucc::ASBR::messageInfo::path()))) {
-                    Parse_Binary((nucc::ASBR::messageInfo*)nullptr);
+                    nucc::ASBR::messageInfo messageInfo{buffer.data(), (size_t)-1, chunk.path.substr(6, 3)};
+                    std::ofstream output(page_directory / std::format("{:03} - {}.json", chunk_index, chunk.name));
+                    output << messageInfo.write_to_json("./messageInfo_hashlist.bin").dump(json_spacing);
+                    output.close();
                 } else if (chunk.path == nucc::ASBR::PlayerColorParam::path()) {
                     Parse_Binary((nucc::ASBR::PlayerColorParam*)nullptr);
                 } else if (chunk.path == nucc::ASBR::SpeakingLineParam::path()) {
